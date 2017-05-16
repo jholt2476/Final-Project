74f209323faa2b14094a7a7da4e33a672df2a40e
jholt2476 <Jackson@Jacksons-MacBook-Pro.local>
2017-05-16 17:59:19 -0400

    Commit
    May 16th still struggling with float and in-line block.

---
+++ main.js
@@ -0,0 +1,25 @@
+var givenName;
+console.log(givenName);
+
+givenName = "Brett";
+console.log(givenName);
+
+givenName = givenName;
+console.log(givenName);
+
+var question;
+console.log(question);
+
+question="HelloHowAreYou";
+console.log(question);
+
+var greeting= question+givenName;
+console.log(greeting);
+
+var high=50;
+var low=10;
+
+var math = high-low
+console.log(math);
+math= high-"5"
+console.log(math);
